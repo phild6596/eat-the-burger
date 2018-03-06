c5874efbeb8e5e70e4af82d15ac0a2fa6719d2f4
phild6596 <phild6596@gmail.com>
2018-03-05 19:48:19 -0800

    added server files, getting an error on connection, troubleshooting the issue.

--- server.js
+++ server.js
@@ -1,3 +1,22 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
+
+const port = process.env.PORT || 3000;
+const app = express();
+
+app.use(express.static(process.cwd() + './public'));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(methodOverride('_method'));
+
+const exphbs = require ('express-handlebars');
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+const routes = require('./controllers/burgers_controller.js');
+
+app.use('/', routes);
+app.listen(port,function ()  {
+    console.log("Listening on port: " + port);
+});
