a7d763afab60d1457c620cd002793c1c2177e68d
phild6596 <phild6596@gmail.com>
2018-03-07 17:06:47 -0800

    added router.delete and router.put to burgers_controller.js

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -12,12 +12,35 @@ router.get('/', function (req, res) {
         res.render('index', hbsObject);
     });
 })
- router.post('/', function (req, res) {
-     burger.create([
-         'burger_name', 'devoured'
-     ], [
-         req.body.burger_name, req.body.devoured
-     ], function(){
-         res.redirect('/');
-     });
- })
+
+router.post('/', function (req, res) {
+    burger.create([
+        'burger_name', 'devoured'
+    ], [
+        req.body.burger_name, req.body.devoured
+    ], function(){
+        res.redirect('/');
+    });
+})
+
+router.put('/:id', function (req, res) {
+    let condition = 'id = ' + req.params.id;
+
+    console.log('condition ' + condition);
+
+    burger.update({
+        devoured: req.body.devoured
+    }, condition, function (){
+        res.redirect('/');
+    });
+})
+
+router.delete('/:id', function (req, res) {
+    let condition = 'id = ' + req.params.id;
+
+    burger.delete(condition, function() {
+        res.redirect('/');
+    });
+})
+
+module.exports = router;
